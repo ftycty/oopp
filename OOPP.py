9415bf3828dc58cdc4f63ba5846daf5edec3820f
Hazim <161602Q@mymail.nyp.edu.sg>
2017-11-20 13:01:41 +0800

    Full website

--- OOPP.py
+++ OOPP.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def home():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
