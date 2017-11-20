051d2eb2d61acd35e1550496dd81118a2d38a304
Hazim <161602Q@mymail.nyp.edu.sg>
2017-11-20 13:30:27 +0800

    Full website v1.1

--- OOPP.py
+++ OOPP.py
@@ -7,6 +7,42 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/amenities')
+def amenities():
+    return render_template('amenities.html')
+
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template('contact_us.html')
+
+@app.route('/food_recipes')
+def food_recipes():
+    return render_template('food_recipes.html')
+
+@app.route('/gym_fitness')
+def gym_fitness():
+    return render_template('gym_fitness.html')
+
+@app.route('/medshop')
+def medshop():
+    return render_template('medshop.html')
+
+@app.route('/my_profile')
+def my_profile():
+    return render_template('my_profile.html')
+
+@app.route('/workouts')
+def workouts():
+    return render_template('workouts.html')
+
 
 if __name__ == '__main__':
     app.run()
