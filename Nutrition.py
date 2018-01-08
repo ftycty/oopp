08b1dcccaa19cafbd04231852d0f40fac04fec9f
hoshangwee <hoshangwee0911@gmail.com>
2018-01-08 12:18:09 +0800

    minor edit

--- Nutrition.py
+++ Nutrition.py
@@ -1,9 +1,13 @@
 from Forum import Forum
 
 class Nutrition(Forum):
-    def __init__(self, title, content, type, ingredient):
+    def __init__(self, title, content, type, ingredient, method):
         Forum.__init__(self, title, content, type)
         self.__ingredient = ingredient
+        self.__method = method
 
     def get_ingredient(self):
         return self.__ingredient
+
+    def get_method(self):
+        return self.__method
